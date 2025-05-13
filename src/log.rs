05c7c81490290df5b1b7c465ff75e3dc1a544952
Jesse Smith <jesses2025smith@gmail.com>
2025-05-13 10:24:10 +0800

    fix log crate dependency

--- src/log.rs
+++ src/log.rs
@@ -33,8 +33,6 @@ macro_rules! trace {
     ($($x:expr),*) => {{
         #[cfg(debug_assertions)]
         println!("\x1b[95m[ TRACE] - {}\x1b[0m", format_args!($($x),*));
-        #[cfg(not(debug_assertions))]
-        log::trace!($($x),*);
     }};
 }
 
@@ -43,8 +41,6 @@ macro_rules! debug {
     ($($x:expr),*) => {{
         #[cfg(debug_assertions)]
         println!("\x1b[96m[ DEBUG] - {}\x1b[0m", format_args!($($x),*));
-        #[cfg(not(debug_assertions))]
-        log::debug!($($x),*);
     }};
 }
 
@@ -53,27 +49,19 @@ macro_rules! info {
     ($($x:expr),*) => {{
         #[cfg(debug_assertions)]
         println!("\x1b[32m[  INFO] - {}\x1b[0m", format_args!($($x),*));
-        #[cfg(not(debug_assertions))]
-        log::info!($($x),*);
     }};
 }
 
 #[macro_export]
 macro_rules! warn {
     ($($x:expr),*) => {{
-        #[cfg(debug_assertions)]
         println!("\x1b[33m[  WARN] - {}\x1b[0m", format_args!($($x),*));
-        #[cfg(not(debug_assertions))]
-        log::warn!($($x),*);
     }};
 }
 
 #[macro_export]
 macro_rules! error {
     ($($x:expr),*) => {{
-        #[cfg(debug_assertions)]
         println!("\x1b[31m[ ERROR] - {}\x1b[0m", format_args!($($x),*));
-        #[cfg(not(debug_assertions))]
-        log::error!($($x),*);
     }};
 }
